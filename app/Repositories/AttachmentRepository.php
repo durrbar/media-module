c7da8e704f76298d2d13537ca6f78822a302fb99
Kid Max <kidmax285@gmail.com>
2026-04-02 23:23:14 +0600

    refactor(media): type attachment repository model signature

--- app/Repositories/AttachmentRepository.php
+++ app/Repositories/AttachmentRepository.php
@@ -12,7 +12,7 @@ class AttachmentRepository extends BaseRepository
     /**
      * Configure the Model
      **/
-    public function model()
+    public function model(): string
     {
         return Attachment::class;
     }
